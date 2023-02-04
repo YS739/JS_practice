c86f9ca05fc6fd31162fef7eb3cda4cf57214105
YS739 <suk921@gmail.com>
2023-02-04 10:46:27 +0900

    Callback

--- js/callback.js
+++ js/callback.js
@@ -3,3 +3,12 @@ var newArr = [10, 20, 30].map(function (currentValue, index) {
   return currentValue + 5;
 });
 console.log(newArr);
+
+// 과제 - 콜백을 이용한 움직이는 원
+// showCircle(150, 150, 100, (div) => {
+//   div.classList.add("message-ball");
+//   div.append("안녕하세요!");
+
+//   // ???
+//   div.onload = () => callback(div);
+// });
